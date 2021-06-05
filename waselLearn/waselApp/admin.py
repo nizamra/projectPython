d6f02f55bfd59848c2b96671da9405c6107914fa
nizamra <nizamra_95@hotmail.com>
2021-06-05 11:03:26 +0300

    added the admin page

--- waselLearn/waselApp/admin.py
+++ waselLearn/waselApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Sugestion, Session, dayTimeSlot
 
 # Register your models here.
+admin.site.register(Sugestion)
+admin.site.register(Session)
+admin.site.register(dayTimeSlot)
